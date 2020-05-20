119b02c7228f0a69773ed4daaec56f48faf954c3
Jean Pierre <jp.irumva@gmail.com>
2020-05-20 17:01:37 +0200

    Dashboard component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import {handleInitialData} from '../actions/shared'
+import Dashboard from './Dashboard'
+
 
 class App extends Component {
+  componentDidMount(){
+    this.props.dispatch(handleInitialData())
+  }
   render() {
     return (
       <div>
-        Starter Code
+        {this.props.loading===true? <div className="center">
+				<p>Wait for Data patiently, it won't take long!</p>
+				<img src="https://media.giphy.com/media/y1ZBcOGOOtlpC/200.gif"/>
+				</div>:
+          <Dashboard/>
+        }
+        
       </div>
     )
   }
 }
+function mapStateToProps({authedUser}){
+  return{
+    loading: authedUser===null
+  }
+}
 
-export default App
+export default connect(mapStateToProps) (App)
